e00bf1aafd7529bfcce82543ec80f4822b3eba97
Maurizio Attanasi <maurizio@boresrl.com>
2020-01-17 15:11:05 +0100

    [master] - Issue 48 code lists generation

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace json_onix_codelists
+{
+    class Program
+    {
+        class CodeList
+        {
+            public string Code { get; set; }
+
+            public string Heading { get; set; }
+
+            public string Note { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            string[] filePaths = Directory.GetFiles(@"data\codelists\html");
+            for (int i = 0; i < filePaths.Length; i++)
+            {
+                using (var reader = new StreamReader(filePaths[i]))
+                {
+                    var html = reader.ReadToEnd();
+
+                    var htmlDoc = new HtmlDocument();
+                    htmlDoc.LoadHtml(html);
+
+                    List<List<string>> table = htmlDoc.DocumentNode.SelectSingleNode("//table")
+                                .Descendants("tr")
+                                .Skip(1)
+                                .Where(tr => tr.Elements("td").Count() > 1)
+                                .Select(tr => tr.Elements("td").Select(td => td.InnerText.Trim()).ToList())
+                                .ToList();
+
+                    var items = new List<CodeList>();
+                    foreach (var row in table)
+                    {
+                        items.Add(new CodeList
+                        {
+                            Code = row[0],
+                            Heading = row[1],
+                            Note = row[2]
+                        });
+                    }
+
+                    var json = JsonConvert.SerializeObject(items);
+                    string destPath = Path.Combine(@"data\codelists\json", Path.GetFileName(filePaths[i]).Replace("htm", "json"));
+                    using (var writer = new StreamWriter(destPath))
+                    {
+                        writer.Write(JValue.Parse(json).ToString(Formatting.Indented));
+                    }
+                }
+
+                Console.WriteLine(Path.GetFileName(filePaths[i]));
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # json-onix-codelists
-Provides all the ONIX 3.0 Code Lists in JSON FORMAR
+
+Provides all the ONIX 3.0 Code Lists in JSON format
+
+The html source files are available at the following link [ONIX for Books 3.0.7 Specification + Codelists Issue 48](https://www.editeur.org/files/ONIX%203/ONIX_for_Books_Release3-0_docs+codes_Issue_48.zip)
